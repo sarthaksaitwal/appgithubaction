0b80e50676478a0f510212ca8138e3be382a3f09
sarthaksaitwal <sarthaksaitwal3@gmail.com>
2026-02-02 00:02:12 +0530

    Updated cases

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -2,7 +2,7 @@ from src.math_operation import add,sub
 
 def test_add():
     assert add(2,3) == 5
-    assert add(-1,-1) == 0
+    assert add(-1,-1) == -2
 
 def test_sub():
     assert sub(5,3) == 2
